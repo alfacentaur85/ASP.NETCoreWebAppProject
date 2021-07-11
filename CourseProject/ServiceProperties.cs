51ddf00b9c76606d1eff1f0159c9950f2b021aba
Artur Stepanyants <alfacentaur@yandex.ru>
2021-07-11 18:06:59 +0300

    lesson2

---
+++ CourseProject/ServiceProperties.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    public class ServiceProperties
+    {
+        public string Host { get; set; }
+    }
+}
---
+++ datalayer/ApplicationDataContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace datalayer
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Posts> Posts { get; set; }
+    }
+}
